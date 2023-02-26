5b921a7c01ad54d96fda7d4cd5415d6747f2be4d
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2023-02-26 11:22:53 +0400

    refactoring const

--- hw01/src/script.js
+++ hw01/src/script.js
@@ -1,9 +1,13 @@
-const refreshTimeout = 1000;
+const FIRST_REFRESH_TIMEOUT = 0;
+const DEFAULT_REFRESH_TIMEOUT = 1000;
 let currentData = null;
 
 function displayCurrentData() {
-    currentData = new Date(Date.now())
-    document.getElementById('current-data').innerHTML = currentData;
+  currentData = new Date(Date.now());
+  document.getElementById("current-data").innerHTML = currentData;
 }
 
-setInterval(() => displayCurrentData(), currentData === null ? 0 : refreshTimeout);
+setInterval(
+  () => displayCurrentData(),
+  currentData === null ? FIRST_REFRESH_TIMEOUT : DEFAULT_REFRESH_TIMEOUT
+);
